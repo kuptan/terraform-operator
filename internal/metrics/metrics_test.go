7810fb6d238c9898885fa0a1ce1b82b5ea89247c
ibraheem Al Saady <ibraheemalsaady@ibraheems-MacBook-Pro-2.local>
2022-08-12 10:37:44 +0200

    fix unit tests

--- internal/metrics/metrics_test.go
+++ internal/metrics/metrics_test.go
@@ -41,7 +41,7 @@ var _ = Describe("Metrics Recorder", func() {
 		})
 
 		Context("Recording Status", func() {
-			It("should record the status metric", func() {
+			It("should record the completed status", func() {
 				rec.RecordStatus(name, namespace, v1alpha1.RunCompleted)
 
 				var (
@@ -60,7 +60,7 @@ var _ = Describe("Metrics Recorder", func() {
 			})
 
 			It("should record the failed status", func() {
-				rec.RecordStatus(name, namespace, v1alpha1.RunFailed)
+				rec.RecordStatus(name+"failed", namespace, v1alpha1.RunFailed)
 
 				var (
 					value      float64 = 1.0
@@ -75,24 +75,6 @@ var _ = Describe("Metrics Recorder", func() {
 				Expect(metricFamilies[0].Metric).To(HaveLen(2))
 				Expect(metricFamilies[0].Metric[1].Gauge).ToNot(BeNil())
 				Expect(metricFamilies[0].Metric[1].Gauge.Value).To(Equal(&value))
-			})
-
-			It("should record the destroyed status", func() {
-				rec.RecordStatus(name, namespace, v1alpha1.RunFailed)
-
-				var (
-					value      float64 = -1.0
-					metricName string  = "tfo_workflow_status"
-				)
-
-				metricFamilies, err := reg.Gather()
-
-				Expect(err).ToNot(HaveOccurred())
-				Expect(metricFamilies).To(HaveLen(2))
-				Expect(metricFamilies[0].Name).To(Equal(&metricName))
-				Expect(metricFamilies[0].Metric).To(HaveLen(3))
-				Expect(metricFamilies[0].Metric[2].Gauge).ToNot(BeNil())
-				Expect(metricFamilies[0].Metric[2].Gauge.Value).To(Equal(&value))
 			})
 		})
 
